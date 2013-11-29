152a79bd7d94aee7d72d2d152b374b3631a2b1ff
George <liusw@carit.com.cn>
2013-11-29 10:45:42 +0800

    add holo stayle

--- src/com/george/obdreader/MaintenanceLog.java
+++ src/com/george/obdreader/MaintenanceLog.java
@@ -1,7 +1,22 @@
 package com.george.obdreader;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MaintenanceLog extends Fragment {
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        Log.d("=====>", "AppleFragment onCreateView");
+        View root = inflater.inflate(R.layout.maintenance_log, container, false);
+        
+        return root;
+    }
+
 
 }
